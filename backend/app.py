aadde21169d459715f567cfbb4670ab9d149d189
Gladys Oviawe <gladysgodwin79@gmail.com>
2023-03-15 13:23:05 +0000

    done

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
-@app.route('/app.py', methods=['POST'])
-def submit_form():
+# Route to show the contact form
+@app.route('/api/contact')
+def contact_form():
+    return render_template('index.html')
+
+# Route to submit the contact form
+@app.route('/api/submit_contact', methods=['POST'])
+def submit_contact():
     name = request.form['name']
     email = request.form['email']
     message = request.form['message']
 
-    # Do something with the data
+    # Do something with the data, e.g. save to database or send notification
 
-    return 'Thank you for submitting the form!'
+    # Return a response to the client
+    response = {
+        'name': name,
+        'email': email,
+        'message': message,
+        'status': 'success'
+    }
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run()
